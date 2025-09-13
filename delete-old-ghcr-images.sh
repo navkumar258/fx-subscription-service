6428ca14aa59aeff3a9ba9d3d0a34e753e347a00
Navneet Kumar <navneet.kumar@accredible.com>
2025-09-14 00:27:28 +0100

    Add CodeQL analysis workflow

---
+++ delete-old-ghcr-images.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+USER="navkumar258"
+PACKAGE="fx-subscription-service"
+TOKEN="${FX_GITHUB_TOKEN}"
+
+# API endpoint: use /orgs or /users as needed
+API="https://api.github.com/users/$USER/packages/container/$PACKAGE/versions?per_page=100"
+
+# Get all but the last 4 version IDs
+version_ids=$(curl -s -H "Authorization: Bearer $TOKEN" "$API" | jq '.[4:] | .[].id')
+
+for id in $version_ids; do
+  echo "Deleting version id: $id"
+  curl -X DELETE -H "Authorization: Bearer $TOKEN" \
+    "https://api.github.com/users/$USER/packages/container/$PACKAGE/versions/$id"
+done
